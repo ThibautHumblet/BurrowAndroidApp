5cb8bafb6282788fe80dc01ec7345a2b72838112
Olivier Verhaegen <olivierverhaegen1@hotmail.com>
2019-12-15 21:32:45 +0100

    Created Add page

--- app/src/main/java/com/example/mobdev_project/AddFragment.java
+++ app/src/main/java/com/example/mobdev_project/AddFragment.java
@@ -1,22 +1,200 @@
 package com.example.mobdev_project;
 
+import android.Manifest;
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import pl.aprilapps.easyphotopicker.ChooserType;
+import pl.aprilapps.easyphotopicker.DefaultCallback;
+import pl.aprilapps.easyphotopicker.EasyImage;
+import pl.aprilapps.easyphotopicker.MediaFile;
+import pl.aprilapps.easyphotopicker.MediaSource;
+
 public class AddFragment extends Fragment {
+    final private Calendar myCalendar = Calendar.getInstance();
+    final private SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
 
-    @Nullable
+    private EditText txtEndDate;
+    private EditText txtNotifyDate;
+    private Button btnChooseImage;
+    private ImageView imgCoupon;
+
+    private static final int CHOOSER_PERMISSIONS_REQUEST_CODE = 7459;
+    private static final int CAMERA_REQUEST_CODE = 7500;
+    private static final int CAMERA_VIDEO_REQUEST_CODE = 7501;
+    private static final int GALLERY_REQUEST_CODE = 7502;
+    private static final int DOCUMENTS_REQUEST_CODE = 7503;
+
+    private EasyImage easyImage;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return  inflater.inflate(R.layout.fragment_add, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return  inflater.inflate(R.layout.fragment_add, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        easyImage = new EasyImage.Builder(getContext())
+                .setChooserTitle("Pick image")
+                .setCopyImagesToPublicGalleryFolder(false)
+                .setChooserType(ChooserType.CAMERA_AND_GALLERY)
+                .setFolderName("Burrow_images")
+                .allowMultiple(false)
+                .build();
+
+        txtEndDate = view.findViewById(R.id.txtEndDate);
+        txtNotifyDate = view.findViewById(R.id.txtNotifyDate);
+        btnChooseImage = view.findViewById(R.id.btnChooseImage);
+        imgCoupon = view.findViewById(R.id.imgCoupon);
+
+        btnChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] necessaryPermissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                if (arePermissionsGranted(necessaryPermissions)) {
+                    easyImage.openChooser(getActivity());
+                } else {
+                    requestPermissionsCompat(necessaryPermissions, CHOOSER_PERMISSIONS_REQUEST_CODE);
+                }
+            }
+        });
+
+        final DatePickerDialog.OnDateSetListener onEndDateSet = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, day);
+
+                setEndDate(myCalendar.getTime());
+            }
+        };
+
+        final DatePickerDialog.OnDateSetListener onNotifyDateSet = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, day);
+
+                setNotifyDate(myCalendar.getTime());
+            }
+        };
+
+        txtEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(getActivity(),
+                        onEndDateSet,
+                        myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        txtNotifyDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(getActivity(),
+                        onNotifyDateSet,
+                        myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void setEndDate(Date endDate) {
+        txtEndDate.setText(dateFormatter.format(endDate));
+    }
+
+    private void setNotifyDate(Date notifyDate) {
+        txtNotifyDate.setText(dateFormatter.format(notifyDate));
+    }
+
+    private boolean arePermissionsGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(getActivity(), permission) != PackageManager.PERMISSION_GRANTED)
+                return false;
+
+        }
+        return true;
+    }
+
+    private void requestPermissionsCompat(String[] permissions, int requestCode) {
+        requestPermissions(permissions, requestCode);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CHOOSER_PERMISSIONS_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openChooser(getActivity());
+        } else if (requestCode == CAMERA_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openCameraForImage(getActivity());
+        } else if (requestCode == CAMERA_VIDEO_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openCameraForVideo(getActivity());
+        } else if (requestCode == GALLERY_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openGallery(getActivity());
+        } else if (requestCode == DOCUMENTS_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openDocuments(getActivity());
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("Pffff", "onActivityResult: TestTESTETET");
+
+        easyImage.handleActivityResult(requestCode, resultCode, data, getActivity(), new DefaultCallback() {
+            @Override
+            public void onMediaFilesPicked(MediaFile[] imageFiles, MediaSource source) {
+                for (MediaFile imageFile : imageFiles) {
+                    Log.d("EasyImage", "Image file returned: " + imageFile.getFile().toString());
+                }
+
+            }
+
+            @Override
+            public void onImagePickerError(@NonNull Throwable error, @NonNull MediaSource source) {
+                //Some error handling
+                error.printStackTrace();
+            }
+
+            @Override
+            public void onCanceled(@NonNull MediaSource source) {
+                //Not necessary to remove any files manually anymore
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mobdev_project/MainActivity.java
+++ app/src/main/java/com/example/mobdev_project/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import pl.aprilapps.easyphotopicker.ChooserType;
+import pl.aprilapps.easyphotopicker.DefaultCallback;
+import pl.aprilapps.easyphotopicker.EasyImage;
+import pl.aprilapps.easyphotopicker.MediaFile;
+import pl.aprilapps.easyphotopicker.MediaSource;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,8 +60,45 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
     }
 
-    public void launchAddOthers(View view) {
-        Fragment fragment = new AddBorrowedOthersFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    // Don't delete needed to call onRequestPermissionsResult in Fragments
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    // Workaround because onActivityResult isn't called in Fragment
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        EasyImage easyImage = new EasyImage.Builder(this)
+                .setChooserTitle("Pick image")
+                .setCopyImagesToPublicGalleryFolder(false)
+                .setChooserType(ChooserType.CAMERA_AND_GALLERY)
+                .setFolderName("Burrow_images")
+                .allowMultiple(false)
+                .build();
+
+        easyImage.handleActivityResult(requestCode, resultCode, data, this, new DefaultCallback() {
+            @Override
+            public void onMediaFilesPicked(MediaFile[] imageFiles, MediaSource source) {
+                for (MediaFile imageFile : imageFiles) {
+                    Log.d("EasyImage", "Image file returned: " + imageFile.getFile().toString());
+                }
+
+                ImageView imgCoupon = findViewById(R.id.imgCoupon);
+                imgCoupon.setImageURI(Uri.fromFile(imageFiles[0].getFile()));
+            }
+
+            @Override
+            public void onImagePickerError(@NonNull Throwable error, @NonNull MediaSource source) {
+                //Some error handling
+                error.printStackTrace();
+            }
+
+            @Override
+            public void onCanceled(@NonNull MediaSource source) {
+                //Not necessary to remove any files manually anymore
+            }
+        });
     }
 }
